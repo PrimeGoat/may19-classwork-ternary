1c5dfb778ef1b3d8a6f7ac0cebe96b7386f72388
PrimeGoat <60146830+PrimeGoat@users.noreply.github.com>
2020-05-26 11:40:28 -0400

    Create main.js

---
+++ main.js
@@ -0,0 +1,43 @@
+//1.
+// const person = [{ name: "Jill" }, { name: "" }, { name: "Bob" }];
+//create a function 'greeting' that maps through the array.
+// Use a ternary.
+// If there is a name it should say 'Hello <name>' otherwise it should say,
+// 'Hello Stranger'.
+//2.
+///Create a function that uses ternary to decide whether the person can drive
+// If they are below 15 then log 'cannot drive', if they are 15 then log 'can drive with a parent', if they are 16 or above then 'yes they can drive'
+//3.
+// create a variable speedMessage that has a value resulting from a ternary
+// if the speed equals 70 it should say '70 is the perfect speed'
+// if the speed is greater than or equal to 90 it should say 'Are you trying to get us killed'
+// if the speed is less than 50 it should have the value 'Faster please'
+// if speed is less than 10 it should say you should not drive
+// there should be a default value that says (if speed is 75 for example), '75 is a decent enough speed'
+// ---
+//4. 
+// map through an array and if the value is greater than 50 it goes in the arr1
+//if the array is less than 50 it goes in arr2
+// if the value is a string arr3
+//floating point numbers should only have 2 decimal places with the exact numbers not rounded
+//use ternary
+// use an array method BUT do not return a data structure when you iterate through your items.
+// OUTPUT:
+//[ 445, 4555 ][ 22, 33.99, 1, 44.98 ] [ 'peaches', 'apple' ]
+let arr = [
+    'peaches',
+    444.7,
+    22,
+    undefined,
+    33.99999,
+    null,
+    1,
+    'apple',
+    4555,
+    undefined,
+    NaN,
+    44.98999,
+  ]
+  let arr1 = []
+  let arr2 = []
+  let arr3 = []
